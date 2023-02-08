77db420ed86ab97acc978e5c07109092a3bdb59d
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2023-02-08 11:25:07 -0500

    added style to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,16 @@ import { LinkContainer } from "react-router-bootstrap";
 import AppNavbar from "../components/AppNavbar";
 import "../styles/Home.css";
 
-
 const Home = () => {
   return (
     <>
       <AppNavbar />
       <Container className="d-flex align-items-center justify-content-center custom-container">
         <div className="w-100 custom-div" style={{ maxWidth: "500px" }}>
-          <Card className="custom-card" style={{ backgroundColor: "white" }}>
+          <Card
+            className="custom-card"
+            style={{ backgroundColor: "rgb(35, 38, 46)", border: "rgb(35, 38, 46" }}
+          >
             <Card.Body className="custom-body">
               Of course it is happening inside your head, but why on earth
               should that mean that it is not real?
